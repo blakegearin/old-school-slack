7c7182eba687a28e1aff1e817a1b409a6bfe1425
Blake Gearin <hello@blakeg.me>
2025-04-29 23:12:55 -0600

    Release commit for 1.0.0

--- src/old-school-slack.user.js
+++ src/old-school-slack.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Old School Slack
 // @namespace    https://github.com/blakegearin/old-school-slack
-// @version      0.1.1
+// @version      1.0.0
 // @description  Updates Slack to look & feel like the old design
 // @author       Blake Gearin <hello@blakeg.me> (https://blakegearin.com)
 // @match        *://app.slack.com/*
@@ -16,7 +16,7 @@
 (function () {
   'use strict';
 
-  const VERSION = '0.1.1';
+  const VERSION = '1.0.0';
   const USERSCRIPT_NAME = 'Old School Slack';
 
   const SILENT = 0;
